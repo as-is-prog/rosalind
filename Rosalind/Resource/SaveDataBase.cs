f3c37645364ca9fc85d11e6825604c1f68b2e792
as-is-prog <as-is-prog@outlook.jp>
2019-02-18 19:04:45 +0900

    [add] セーブデータ絡みのコメント追加

--- Rosalind/Resource/SaveDataBase.cs
+++ Rosalind/Resource/SaveDataBase.cs
@@ -9,10 +9,21 @@ using System.Threading.Tasks;
 
 namespace Shiorose.Resource
 {
+    /// <summary>
+    /// セーブデータを管理するクラス
+    /// </summary>
     public static class SaveDataManager
     {
+        /// <summary>
+        /// セーブデータのファイル名
+        /// </summary>
         public static string SaveFileName { get; set; } = "savedata.json";
 
+        /// <summary>
+        /// セーブデータの読み込みを行う
+        /// </summary>
+        /// <typeparam name="T">セーブデータのクラス(BaseSaveDataを継承したクラス)</typeparam>
+        /// <returns>セーブデータのインスタンス</returns>
         public static T Load<T>() where T : BaseSaveData, new()
         {
             if (!File.Exists(Rosalind.ShioriDir + SaveFileName)) return new T();
@@ -24,6 +35,11 @@ namespace Shiorose.Resource
             }
         }
 
+        /// <summary>
+        /// セーブデータの保存を行う
+        /// </summary>
+        /// <typeparam name="T">セーブデータのクラス(BaseSaveDataを継承したクラス)</typeparam>
+        /// <param name="saveData">セーブデータのインスタンス</param>
         public static void Save<T>(T saveData) where T : BaseSaveData, new()
         {
             using (var stream = File.Create(Rosalind.ShioriDir + SaveFileName))
@@ -34,6 +50,9 @@ namespace Shiorose.Resource
         }
     }
 
+    /// <summary>
+    /// セーブデータのクラス。ゴースト開発者はこのクラスを継承して保存したい項目を増やすと良い。
+    /// </summary>
     [DataContract]
     public class BaseSaveData
     {
@@ -43,6 +62,9 @@ namespace Shiorose.Resource
         [DataMember]
         public int TalkInterval { get; set; }
 
+        /// <summary>
+        /// セーブデータをファイルに書き込む
+        /// </summary>
         public void Save()
         {
             SaveDataManager.Save(this);
