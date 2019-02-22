36703dd34e677ffcc7c4e1c6364dc77444b79f28
as-is-prog <as-is-prog@outlook.jp>
2019-02-22 14:01:05 +0900

    [add] ランダムに文字列を選ぶ補助メソッド追加

---
+++ Rosalind/Support/Util.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shiorose.Support
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Util
+    {
+        private static Random random = new Random();
+
+        /// <summary>
+        /// 引数で渡された文字列からランダムに一つ選択し返します。
+        /// </summary>
+        /// <param name="strs">文字列</param>
+        /// <returns></returns>
+        public static string RandomChoice(params string[] strs)
+        {
+            return strs[random.Next(strs.Length)];
+        }
+    }
+}
